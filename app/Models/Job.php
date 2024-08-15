bb0e9c782da4400ebfd1b0c82c073fe2f99c4759
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-08-15 11:03:27 -0300

    Two key eloquent relationship types

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,4 +12,9 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
