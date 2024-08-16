b53801c162805bafc32bb30c304f737ecb78ca96
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-08-16 09:25:06 -0300

    Adding and configuring pagination

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,18 @@
         Jobs Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}</strong> : Pays {{ $job['salary'] }} per year
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                    <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                    <div>
+                        <strong>{{ $job['title'] }}</strong> : Pays {{ $job['salary'] }} per year
+                    </div>
                 </a>
-            </li>
         @endforeach
-    </ul>
+
+        <div>
+            {{ $jobs->links() }}
+        </div>
+    </div>
 </x-layout>
