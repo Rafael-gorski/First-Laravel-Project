a548ef221f4eb608bcb99bf2bfbc29beb98a8069
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-08-18 19:48:02 -0300

    Create button and a validation for the form

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/jobs/create', function () {
 });
 
 Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'min:4'],
+    ]);
+
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
